8fb8b8c7300a7a385f3090ba9632075158f1ea19
Akshay kumar k <akshayajith40@gmail.com>
2022-11-07 21:05:30 +0530

    admin login and homepage finished and userdetails added to admin homepage from database

--- helpers/admin_helper.js
+++ helpers/admin_helper.js
@@ -1,5 +1,10 @@
 const db = require("../confi/connection");
 
 module.exports ={
-    
+    getAllUsers:()=>{
+         return new Promise(async(resolve,reject)=>{
+            let allUsers = await db.get().collection('users').find().toArray();
+            resolve(allUsers);
+         });
+    }
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,42 +1,47 @@
 const express = require("express");
+const admin = require("../helpers/admin_helper");
+const db = require("../confi/connection");
 const router = express.Router();
-
 const loginDetails = {
-    email:"akshayajith@gmail.com",
-    password:"akshay123"
+    email:"akshay@a",
+    password:"12"
 };
 
 router.get("/", (req, res, next) => {
-  session = req.session;
-  if (session.adminId) {
-    res.render("admin/homepage_admin");
-  } else {
+    session = req.session;
+    if (session.adminId) {
+    res.redirect('/admin/adminhome');
+    } else {
     res.render("admin/login_admin");
-  }
+    }
 });
-router.post('/adminlogin',(req,res)=>{
+router.get("/adminhome", (req, res) => {
+  admin.getAllUsers().then((allUsers) => {
+    console.log(allUsers);
+    session = req.session;
+    if (session.adminId) {
+      res.render("admin/homepage_admin", { allUsers });
+    } else {
+      res.redirect('/admin');
+    }
+  });
+});
+router.post('/adminlogin',(req,res)=>{  
     if (
       req.body.email === loginDetails.email &&
       req.body.password === loginDetails.password
     ) {
-      session = req.session;
-      session.adminId = req.body.email;
+      req.session.adminId = req.body.email;
       console.log(req.session);
-      res.redirect('adminhome');
+      res.redirect('/admin/adminhome');
     } else {
       res.render("admin/login_admin", { err_message: "username or password incorrect" });
     }
+
 })
-router.get('/adminhome',(req,res)=>{
-     session = req.session;
-     if (session.adminId) {
-       res.render('admin/homepage_admin');
-     } else {
-       res.render("admin/login_admin");
-     }
-})
+
 router.get("/adminlogout", (req, res) => {
   req.session.destroy();
   res.redirect("/admin");
-});
+});    
 module.exports = router;
